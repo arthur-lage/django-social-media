e60eb37559dc90d6efcbbd2847ab22fe9c64e883
Arthur Lage <arthurlage2006@gmail.com>
2022-10-15 14:04:37 -0300

    initial commit

---
+++ core/views.py
@@ -0,0 +1,92 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required(login_url="signin")
+def index (request):
+  return render(request, "index.html")
+
+def signup (request):
+  if request.method == "POST":
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    if password == password2:
+      if User.objects.filter(email=email).exists():
+        messages.info(request, "This email is already being used!")
+        return redirect("signup")
+
+      if User.objects.filter(username=username).exists():
+        messages.info(request, "This username is already being used!")
+        return redirect("signup")
+
+      user = User.objects.create_user(username=username, email=email, password=password)
+      user.save()
+
+      user_login = auth.authenticate(username=username, password=password)
+      auth.login(request, user_login)
+
+      user_model = User.objects.get(username=username)
+
+      new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+      new_profile.save()
+
+      return redirect('settings')
+
+    else:
+      messages.info(request, 'Passwords should match!')
+      return redirect('signup')
+  else:
+    return render(request, "signup.html")
+
+def signin (request):
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+      auth.login(request, user)
+      return redirect("/")
+    else:
+      messages.info(request, "Invalid credentials!")
+      return redirect("signin")
+
+  else:
+    return render(request, "signin.html")
+
+@login_required(login_url="signin")
+def logout (request):
+  auth.logout(request)
+  return redirect("signin")
+
+@login_required(login_url="signin")
+def settings (request):
+  user_profile = Profile.objects.get(user=request.user)
+
+  if request.method == "POST":
+    image = ""
+    bio = request.POST["bio"]
+    location = request.POST["location"]
+
+    if request.FILES.get('image') == None:
+      image = user_profile.profileImg
+    elif request.FILES.get('image') != None:
+      image = request.FILES.get("image")
+    
+    user_profile.profileImg = image
+    user_profile.bio = bio
+    user_profile.location = location
+    user_profile.save()
+
+    return redirect("settings")
+
+  return render(request, "setting.html", {"user_profile": user_profile})
